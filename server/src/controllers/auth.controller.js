a9c612c8df603f19ba9b9dbbbc763fa19ce818c2
juyalrohit <rohitjuyalp205@gmail.com>
2026-06-12 10:16:21 +0530

    Fix deployment error

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -92,11 +92,15 @@ export const login = async (req, res) => {
     const token = generateToken({userId : user._id}, '7d');
 
      res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite:
+      process.env.NODE_ENV === "production"
+        ? "none"
+        : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
 
     res.status(200).json({
       success: true,
